6a63db06fa2cb6fd94654cf7001e9092366883f7
Chemcem <chemicraftdev@gmail.com>
2025-09-26 08:28:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1981,13 +1981,12 @@ let currentLanguage = "en";
 
 function setLanguage(lang) {
   currentLanguage = lang;
-document.querySelectorAll("[data-i18n]").forEach((element) => {
-  const key = element.getAttribute("data-i18n");
-  if (translations[lang] && translations[lang][key]) {
-    element.innerHTML = translations[lang][key];
-  }
-});
-
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
+    const key = element.getAttribute("data-i18n");
+    if (translations[lang] && translations[lang][key]) {
+      element.innerHTML = translations[lang][key];
+    }
+  });
 
   document.querySelectorAll("[data-i18n-placeholder]").forEach((element) => {
     const key = element.getAttribute("data-i18n-placeholder");
@@ -1995,8 +1994,6 @@ document.querySelectorAll("[data-i18n]").forEach((element) => {
       element.placeholder = translations[lang][key];
     }
   });
-
-
 
   document.querySelectorAll("[data-i18n-title]").forEach((element) => {
     const key = element.getAttribute("data-i18n-title");
@@ -5770,5 +5767,3 @@ openDevProfilesBtn.addEventListener("click", () => {
 closeDevProfilesBtn.addEventListener("click", () => {
   devProfilesModal.classList.add("hidden");
 });
-
-
